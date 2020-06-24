138ce3e99450f80222a34acf092d98c3de66bf7d
Andrey Petrikov <aapetrikov@gmail.com>
2020-06-24 14:17:11 +0300

    task 1

--- src/index.js
+++ src/index.js
@@ -1,72 +1,29 @@
 //task 1
-const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40 };
 
-export default function orderByProps(obj, keys) {
-    let m = [];
-    let s = [];
-    for (const k in obj) {
-        m.push({ key: k, value: obj[k] });
-    };
-
-    keys.forEach(element => {
-        s.push({ key: element, value: obj[element] });
-    });
-
-    m = sorter(m);
-    m.forEach(el => {
-        if (s.find(ithem => ithem.key === el.key) === undefined) {
-            s.push(el);
-        }
-    });
-
-    return s;
+export default function Character(name, type) {
+    let typeCharacter = [
+        "Bowman", "Swordsman", "Magician", "Daemon", "Undead", "Zombie"
+    ];
+    if (name.lenght < 1 || name.lenght > 11) {
+        throw new Error("Имя должно быть от 2 до 10 символов");
+    }
+    if (!typeCharacter.includes(type)) {
+        throw new Error("Тип персонажа не найден");
+    }
+    this.name = name;
+    this.type = type;
+    this.health= 100;
+    this.level= 1;
+    this.Bowman= "25/25";
+    this.Swordsman= "40/10";
+    this.Magician= "10/40";
+    this.Undead= "25/25";
+    this.Zombie= "40/10";
+    this.Daemon= "10/40";
 }
+
+
 
 export function sorter(list) {
-    list.sort(function (a, b) {
-        var nameA = a.key.toLowerCase(), nameB = b.key.toLowerCase();
-        if (nameA < nameB)
-            return -1
-        if (nameA > nameB)
-            return 1
-        return 0
-    });
-    return list;
+
 }
-
-// orderByProps(obj, ["name", "level"]);
-
-// task 2
-let hits = {
-    name: 'Лучник',
-    type: 'Bowman',
-    health: 50,
-    level: 3,
-    attack: 40,
-    defence: 10,
-    special: [
-        {
-            id: 8,
-            name: 'Двойной выстрел',
-            icon: 'http://...',
-            description: 'Двойной выстрел наносит двойной урон'
-        },
-        {
-            id: 9,
-            name: 'Нокаутирующий удар',
-            icon: 'http://...'
-            // <- обратите внимание, описание "засекречено"
-        }
-    ]
-};
-
-export function getHits(hits) {
-    let listHits = [];
-
-    for (let { id, name, icon, description = "Описание недоступно" } of hits.special) {
-        listHits.push([id, name, icon, description]);
-    }
-    return listHits
-}
-
-// getHits(hits);
--- src/test.js
+++ src/test.js
@@ -1,70 +1,76 @@
-import { getHits, sorter } from "../src/index.js";
-import orderByProps from "../src/index.js";
+// import { getHits, sorter } from "../src/index.js";
+import Character from "../src/index.js";
 
-test("sort with condition", () => {
-    const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40 };
-    const result = orderByProps(obj, ["name", "level"]);
+test("new Character", () => {
+    const result = new Character("Тор","Daemon");
     const expected = [
-        { key: "name", value: "мечник" }, // порядок взят из массива с ключами
-        { key: "level", value: 2 }, // порядок взят из массива с ключами
-        { key: "attack", value: 80 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
-        { key: "defence", value: 40 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
-        { key: "health", value: 10 } // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
+        { key: "name", value: "Тор" }, // порядок взят из массива с ключами
+        { key: "type", value: "Daemon" }, // порядок взят из массива с ключами
     ]
     expect(result).toEqual(expected);
 })
-test("sorter", () => {
-    const result = sorter([
-        { key: "name", value: "мечник" }, // порядок взят из массива с ключами
-        { key: "level", value: 2 }, // порядок взят из массива с ключами
-        { key: "attack", value: 80 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
-        { key: "defence", value: 40 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
-        { key: "health", value: 10 } // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
-    ]);
-    const expected = [
-        { key: "attack", value: 80 }, 
-        { key: "defence", value: 40 }, 
-        { key: "health", value: 10 },
-        { key: "level", value: 2 }, 
-        { key: "name", value: "мечник" }
-    ];
-    expect(result).toEqual(expected);
+test("error name Character", () => {
+    const result = new Character("re","Daemon");
+    const expected = "Имя должно быть от 2 до 10 символов;
+    expect(result).toBe(expected);
 })
+test("error type Character", () => {
+    const result = new Character("Tor","Мечник");
+    const expected = "Тип персонажа не найден";
+    expect(result).toBe(expected);
+})
+// test("sorter", () => {
+//     const result = sorter([
+//         { key: "name", value: "мечник" }, // порядок взят из массива с ключами
+//         { key: "level", value: 2 }, // порядок взят из массива с ключами
+//         { key: "attack", value: 80 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
+//         { key: "defence", value: 40 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
+//         { key: "health", value: 10 } // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
+//     ]);
+//     const expected = [
+//         { key: "attack", value: 80 }, 
+//         { key: "defence", value: 40 }, 
+//         { key: "health", value: 10 },
+//         { key: "level", value: 2 }, 
+//         { key: "name", value: "мечник" }
+//     ];
+//     expect(result).toEqual(expected);
+// })
 
-test("getHits", () => {
-    let hits = {
-        name: 'Лучник',
-        type: 'Bowman',
-        health: 50,
-        level: 3,
-        attack: 40,
-        defence: 10,
-        special: [
-            {
-                id: 8,
-                name: 'Двойной выстрел',
-                icon: 'http://...',
-                description: 'Двойной выстрел наносит двойной урон'
-            },
-            {
-                id: 9,
-                name: 'Нокаутирующий удар',
-                icon: 'http://...'
-                // <- обратите внимание, описание "засекречено"
-            }
-        ]
-    };
-    const result = getHits(hits);
-    const expected = [[
-        8,
-        'Двойной выстрел',
-        'http://...',
-        'Двойной выстрел наносит двойной урон'],
-    [
-        9,
-        'Нокаутирующий удар',
-        'http://...',
-        'Описание недоступно'
-    ]]
-    expect(result).toEqual(expected);
-})
+// test("getHits", () => {
+//     let hits = {
+//         name: 'Лучник',
+//         type: 'Bowman',
+//         health: 50,
+//         level: 3,
+//         attack: 40,
+//         defence: 10,
+//         special: [
+//             {
+//                 id: 8,
+//                 name: 'Двойной выстрел',
+//                 icon: 'http://...',
+//                 description: 'Двойной выстрел наносит двойной урон'
+//             },
+//             {
+//                 id: 9,
+//                 name: 'Нокаутирующий удар',
+//                 icon: 'http://...'
+//                 // <- обратите внимание, описание "засекречено"
+//             }
+//         ]
+//     };
+//     const result = getHits(hits);
+//     const expected = [[
+//         8,
+//         'Двойной выстрел',
+//         'http://...',
+//         'Двойной выстрел наносит двойной урон'],
+//     [
+//         9,
+//         'Нокаутирующий удар',
+//         'http://...',
+//         'Описание недоступно'
+//     ]]
+//     expect(result).toEqual(expected);
+// })
