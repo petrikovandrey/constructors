1149649b335ba5e12e6d0631a7c6f45344b599dd
Andrey Petrikov <aapetrikov@gmail.com>
2020-06-24 15:12:59 +0300

    task 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 //task 1
-
 export default function Character(name, type) {
     let typeCharacter = [
         "Bowman", "Swordsman", "Magician", "Daemon", "Undead", "Zombie"
     ];
-    if (name.lenght < 1 || name.lenght > 11) {
+    if (name.length < 2 || name.length > 10) {
         throw new Error("Имя должно быть от 2 до 10 символов");
     }
     if (!typeCharacter.includes(type)) {
@@ -12,18 +11,28 @@ export default function Character(name, type) {
     }
     this.name = name;
     this.type = type;
-    this.health= 100;
-    this.level= 1;
-    this.Bowman= "25/25";
-    this.Swordsman= "40/10";
-    this.Magician= "10/40";
-    this.Undead= "25/25";
-    this.Zombie= "40/10";
-    this.Daemon= "10/40";
+    this.health = 100;
+    this.level = 1;
+    this.Bowman = "25/25";
+    this.Swordsman = "40/10";
+    this.Magician = "10/40";
+    this.Undead = "25/25";
+    this.Zombie = "40/10";
+    this.Daemon = "10/40";
 }
+// task2
+// function Character(name, type) {
+//     this.name = name;
+//     this.type = type;
+//     this.health = 100;
+//     this.attack = 10;
+//     this.defence = 40;
+// }
 
-
-
-export function sorter(list) {
-
+Character.prototype = {
+    damage: (points) => {
+        if (this.health >= 0) {
+            this.health -= points * (1 - this.defence / 100);
+        }
+    }
 }
--- src/test.js
+++ src/test.js
@@ -3,74 +3,24 @@ import Character from "../src/index.js";
 
 test("new Character", () => {
     const result = new Character("Тор","Daemon");
-    const expected = [
-        { key: "name", value: "Тор" }, // порядок взят из массива с ключами
-        { key: "type", value: "Daemon" }, // порядок взят из массива с ключами
-    ]
+    const expected =
+        {"Bowman": "25/25", "Daemon": "10/40", "Magician": "10/40", "Swordsman": "40/10", "Undead": "25/25", "Zombie": "40/10", "health": 100, "level": 1, "name": "Тор", "type": "Daemon"};
     expect(result).toEqual(expected);
 })
 test("error name Character", () => {
-    const result = new Character("re","Daemon");
-    const expected = "Имя должно быть от 2 до 10 символов;
-    expect(result).toBe(expected);
+    const result = new Character("r","Daemon");
+    const expected = "Имя должно быть от 2 до 10 символов";
+    expect(result).toThrow(expected);
 })
 test("error type Character", () => {
     const result = new Character("Tor","Мечник");
     const expected = "Тип персонажа не найден";
+    expect(result).toThrow(expected);
+})
+test("damage ", () => {
+    const person = new Character("Тор","Daemon");
+    person.damage(99);
+    const result = person.health;
+    const expected = 1;
     expect(result).toBe(expected);
 })
-// test("sorter", () => {
-//     const result = sorter([
-//         { key: "name", value: "мечник" }, // порядок взят из массива с ключами
-//         { key: "level", value: 2 }, // порядок взят из массива с ключами
-//         { key: "attack", value: 80 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
-//         { key: "defence", value: 40 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
-//         { key: "health", value: 10 } // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
-//     ]);
-//     const expected = [
-//         { key: "attack", value: 80 }, 
-//         { key: "defence", value: 40 }, 
-//         { key: "health", value: 10 },
-//         { key: "level", value: 2 }, 
-//         { key: "name", value: "мечник" }
-//     ];
-//     expect(result).toEqual(expected);
-// })
-
-// test("getHits", () => {
-//     let hits = {
-//         name: 'Лучник',
-//         type: 'Bowman',
-//         health: 50,
-//         level: 3,
-//         attack: 40,
-//         defence: 10,
-//         special: [
-//             {
-//                 id: 8,
-//                 name: 'Двойной выстрел',
-//                 icon: 'http://...',
-//                 description: 'Двойной выстрел наносит двойной урон'
-//             },
-//             {
-//                 id: 9,
-//                 name: 'Нокаутирующий удар',
-//                 icon: 'http://...'
-//                 // <- обратите внимание, описание "засекречено"
-//             }
-//         ]
-//     };
-//     const result = getHits(hits);
-//     const expected = [[
-//         8,
-//         'Двойной выстрел',
-//         'http://...',
-//         'Двойной выстрел наносит двойной урон'],
-//     [
-//         9,
-//         'Нокаутирующий удар',
-//         'http://...',
-//         'Описание недоступно'
-//     ]]
-//     expect(result).toEqual(expected);
-// })
